cff201c2635936faaad78948f9f17cbccbf33c81
Jason Lee <jason@zerodevx.com>
2022-12-16 17:40:10 +0700

    Add tests for `v1`

--- playwright.config.js
+++ playwright.config.js
@@ -1,8 +1,9 @@
 /** @type {import('@playwright/test').PlaywrightTestConfig} */
 const config = {
   webServer: {
-    command: 'npm run build && npm run preview',
-    port: 4173
+    command: 'npm run dev',
+    port: 5173,
+    reuseExistingServer: true
   },
   testDir: 'tests'
 }
