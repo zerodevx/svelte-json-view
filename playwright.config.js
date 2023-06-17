7e4731baab9cb4ce9433fa2db0d8057d4490130c
Jason Lee <jason@zerodevx.com>
2023-06-17 23:45:05 +0800

    Update test config

--- playwright.config.js
+++ playwright.config.js
@@ -1,11 +1,14 @@
-/** @type {import('@playwright/test').PlaywrightTestConfig} */
-const config = {
+import { defineConfig } from '@playwright/test'
+
+export default defineConfig({
+  testDir: 'tests',
+  testMatch: '**/*.js',
+  use: {
+    baseURL: 'http://localhost:5173'
+  },
   webServer: {
     command: 'npm run dev',
-    port: 5173,
+    url: 'http://localhost:5173',
     reuseExistingServer: true
-  },
-  testDir: 'tests'
-}
-
-export default config
+  }
+})
