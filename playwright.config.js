376d6a8f355fc57b0fe893107e2f3ae1e16dacba
Jason Lee <jason@zerodevx.com>
2026-07-25 17:40:28 +0800

    chore: upgrade `sveltekit` tooling

--- playwright.config.js
+++ playwright.config.js
@@ -1,15 +1,6 @@
-/** @type {import('@playwright/test').PlaywrightTestConfig} */
-const config = {
-  use: {
-    baseURL: 'http://localhost:5173'
-  },
-  webServer: {
-    command: 'npm run dev',
-    url: 'http://localhost:5173',
-    reuseExistingServer: true
-  },
-  testDir: 'tests',
-  testMatch: /(.+\.)?(test|spec)\.[jt]s/
-}
+import { defineConfig } from '@playwright/test'
 
-export default config
+export default defineConfig({
+  webServer: { command: 'npm run build && npm run preview', port: 4173 },
+  testMatch: '**/*.e2e.{ts,js}'
+})
